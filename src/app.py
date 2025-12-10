6ee5eb3b25995ce06e423fadaa9cc36153c7320f
Fernando Del Rio <fernandogondelrio@gmail.com>
2025-12-10 12:26:18 +0000

    Proyecto Flask API

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "Sample Todo 1", "done": True },
+    { "label": "Sample Todo 2", "done": True }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
